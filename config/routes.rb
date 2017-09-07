ce1d4bfdd5f72c1df74f589790a560739e5903fc
Dimitrios J. Vergados <djvergad@gmail.com>
2017-09-07 16:09:29 +0300

    Added user menu dropbown
    Former-commit-id: 97c58233f61547e375295c0a8ffb045db72106be
    Former-commit-id: 79a9a939c846e302d62b1ecb52dcc06a973ab3c5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-  Rails.application.routes.draw do
+Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
+  resources :users
 
-  get '/users/:id', :to => 'users#show', :as => :user
-
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
