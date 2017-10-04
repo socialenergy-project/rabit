54e6e11530eb391b8638f75a24dd0d06a90b11fe
Dimitrios J. Vergados <djvergad@gmail.com>
2017-10-04 18:36:25 +0300

    Improve prosumer category listing
    Former-commit-id: f9375ccd0169b655f4f53e74f990ba7f1e9a6424
    Former-commit-id: 8f7da5413a3e87d129796ef08400ffc7d5f3ae33

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -3,4 +3,6 @@ class Consumer < ApplicationRecord
   belongs_to :connection_type, optional: true
   belongs_to :consumer_category
   has_and_belongs_to_many :communities
+
+  scope :category, ->(cat) { where(consumer_category: cat) if cat.present? }
 end
