ec694aadc0758ac1bac619324b49b28b0f015026
Dimitrios J. Vergados <djvergad@gmail.com>
2017-10-04 14:59:31 +0300

    Add many-to-many relationship in model
    Former-commit-id: e61efe6bceec830af961044f7500f0cb3745ba9d
    Former-commit-id: ff5cbf4fd62529bc791e64ccdcc528488c615a02

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,3 +1,5 @@
 class Community < ApplicationRecord
   belongs_to :clustering
+  has_and_belongs_to_many :consumers
+
 end
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -2,4 +2,5 @@ class Consumer < ApplicationRecord
   belongs_to :building_type, optional: true
   belongs_to :connection_type, optional: true
   belongs_to :consumer_category
+  has_and_belongs_to_many :communities
 end
