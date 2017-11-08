1d5484d9359951ec072da6c16f06b6283a9cd533
Dimitrios J. Vergados <vergados@ceid.upatras.gr>
2017-11-08 10:36:03 +0200

    Asset precompile
    Former-commit-id: 2819ee72fef17196a330d9836a8519dc2e444950
    Former-commit-id: b1658ef542b281cf15dda9ff0127fdb353e0dae0

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# import rbenv
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+
+# enter deploy directory
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+# pull new code
+git pull
+
+# update the database
+RAILS_ENV=production rails db:migrate
+
+# and the static files
+RAILS_ENV=production rails assets:precompile
+
+# and restart the server
+passenger-config restart-app .
+
+
+# commit the new assets back to the repo
+git add .
+git commit -m "Asset precompile"
+git push
+
