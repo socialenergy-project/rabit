4e5fe1a1cd695fb3cdb709b6cedb279966bc5968
Dimitrios J. Vergados <djvergad@gmail.com>
2017-11-20 15:19:07 +0200

    Allow deletion of users with scenarios, set default username
    Former-commit-id: 8a834ff62c4265fd139c05380a20c5f65222a1cd
    Former-commit-id: caf3897d8d1b5ae3616da354d27ecf41320caa25

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_name
+
+  has_many :scenarios, dependent: :nullify
+
+  private
+  def set_name
+    self.name = self.email[/^[^@]+/].split('.').map(&:capitalize).join(' ')
+  end
 end
