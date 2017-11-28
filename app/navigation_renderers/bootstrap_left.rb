29c74601fee2cf7e1a5607e412915462bdd69681
Dimitrios J. Vergados <djvergad@gmail.com>
2017-11-28 13:54:47 +0200

    Added icons to the menu
    Former-commit-id: f5f4d4a23a49b3da1f8893f0d82a1b77fde66151
    Former-commit-id: c3c9feb9dbeacbd82c2b546521b65564d859ccfd

--- app/navigation_renderers/bootstrap_left.rb
+++ app/navigation_renderers/bootstrap_left.rb
@@ -13,5 +13,13 @@ class BootstrapLeft < SimpleNavigation::Renderer::List
     item.sub_navigation.dom_class += " show" if item.selected?
     item.sub_navigation.render(opt)
   end
+
+  def tag_for(item)
+    if item.options[:icon]
+      link_to(content_tag(:i, "", class: item.options[:icon]) + content_tag(:span, item.name, class: 'nav-link-text'), item.url, options_for(item))
+    else
+      super
+    end
+  end
 end
 
