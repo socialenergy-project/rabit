9db61be8998ce5fad426d088ae662d847d087e41
Dimitrios J. Vergados <djvergad@gmail.com>
2018-02-22 09:02:03 +0200

    Working on gsrn auth
    Former-commit-id: 2f2f021d25d20e61dadef87a9956446433b53f4d
    Former-commit-id: 673f2f4678d4f167f520b676a1f3c11304b3f3e8

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,8 @@ Rails.application.routes.draw do
   resources :clusterings
   resources :connection_types
   resources :flexibilities
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   root 'pages#home'
   get 'pages/home'
   resources :users
