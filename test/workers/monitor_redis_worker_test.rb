7d3f7e98c2c88028e1c4bc486c7a210be4fac4f5
Dimitrios J. Vergados <djvergad@gmail.com>
2019-04-19 09:16:20 +0300

    Remove warning in test case

--- test/workers/monitor_redis_worker_test.rb
+++ test/workers/monitor_redis_worker_test.rb
@@ -1,5 +1,5 @@
 require 'test_helper'
-class MonitorRedisWorkerTest < MiniTest::Unit::TestCase
+class MonitorRedisWorkerTest < MiniTest::Test
   def test_example
     skip "add some examples to (or delete) #{__FILE__}"
   end
