e384b9fa50d075de570679a3e18b3c9c3ae29b00
Dimitrios J. Vergados <djvergad@gmail.com>
2019-09-05 18:08:35 +0300

    Remove deadlock

--- lib/fetch_data/group_membership_client.rb
+++ lib/fetch_data/group_membership_client.rb
@@ -7,26 +7,27 @@ module FetchData
     end
 
     def get_all()
-      Parallel.map(User.all) do |user|
+      json_array = Parallel.map(User.all, in_threads: 10) do |user|
         if user.uid
           result = RestClient.post @gsrn_group_membership_url, {
             username: user.uid
           }, timeout: 10
-          json = JSON.parse result
-          p "the results is ", json
-          json.each do |g|
-            Group.find_or_create_by name: g['userGroups']
-          end
-          user.group_memberships = json.map do |g|
-            # if user.uid == g['username']
-              GroupMembership.new user: user,
-                                  group: Group.find_by(name: g['userGroups']),
-                                  role: GroupMembership.roles[g['roleUser'].downcase.gsub(/ /, '_')]
-            # end
-          end.reject(&:nil?)
-          user.save
-          json
+          [user, JSON.parse(result)]
         end
+      end.reject {|u,j| j.nil?}
+      json_array.map do |user,json|
+        json.each do |g|
+          Group.find_or_create_by name: g['userGroups']
+        end
+        user.group_memberships = json.map do |g|
+          # if user.uid == g['username']
+            GroupMembership.new user: user,
+                                group: Group.find_by(name: g['userGroups']),
+                                role: GroupMembership.roles[g['roleUser'].downcase.gsub(/ /, '_')]
+          # end
+        end
+        user.save
+        json
       end
     end
   end
