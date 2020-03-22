18856f95b2ac4b2e8e2f0459920a1c8e93369f5b
Dimitrios J. Vergados <djvergad@gmail.com>
2020-03-22 08:35:45 +0200

    Allow user to see all consumers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -34,8 +34,8 @@ class Ability
     else
       # cannot :index, User
       can :read, :all
-      cannot :read, Consumer
-      can :read, Consumer, users: {id: user.groups.joins(:users).select('users.id').pluck(:'users.id') }
+      # cannot :read, Consumer
+      # can :read, Consumer, users: {id: user.groups.joins(:users).select('users.id').pluck(:'users.id') }
       # can [:select, :confirm], Clustering
       cannot :read, [User]
       can :show, User do |emp|
