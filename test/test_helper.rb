11d7a1855a17e4250d593a1a9832409afc26d8df
Dimitrios J. Vergados <djvergad@gmail.com>
2020-07-27 14:55:19 +0300

    Update request stub

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,10 @@
 require 'simplecov'
 SimpleCov.start
 
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
 require 'webmock/test_unit'
-
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -17,27 +16,27 @@ end
 class ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   setup do
-    host! "localhost:3000"
+    host! 'localhost:3000'
 
-    WebMock.stub_request(:post, "https://socialenergy.intelen.com/index.php/webservices/activitylcmsgame")
+    WebMock.stub_request(:post, 'https://socialenergy.intelen.com/index.php/webservices/activitylcmsgame')
            .with(
-              body: {"username"=>nil},
-              headers: {
-                'Accept'=>'*/*',
-                'Accept-Encoding'=>'gzip, deflate',
-                'Content-Length'=>'8',
-                'Content-Type'=>'application/x-www-form-urlencoded',
-                'Host'=>'socialenergy.intelen.com',
-                'Timeout'=>'10',
-                'User-Agent'=>'rest-client/2.0.2 (linux-gnu x86_64) ruby/2.5.1p57'
-              }
-            ).to_return(status: 200, body: "", headers: {})
-
+             body: { 'username' => nil },
+             headers: {
+               'Accept' => '*/*',
+               'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+               'Content-Length' => '8',
+               'Content-Type' => 'application/x-www-form-urlencoded',
+               'Host' => 'socialenergy.intelen.com',
+               'Timeout' => '10',
+               'User-Agent' => 'rest-client/2.1.0 (linux-gnu x86_64) ruby/2.6.3p62'
+             }
+           )
+           .to_return(status: 200, body: '', headers: {})
   end
 
   def sign_in_as_admin
     user = users(:one)
-    user.add_role "admin"
+    user.add_role 'admin'
     sign_in user
     user
   end
@@ -47,5 +46,4 @@ class ActionDispatch::IntegrationTest
     sign_in user
     user
   end
-
 end
